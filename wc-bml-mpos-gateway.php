163f6f6f415954c072424c05ccf796d5d99a8f0d
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2024-09-18 19:08:01 +0500

    added version and author url

--- wc-bml-mpos-gateway.php
+++ wc-bml-mpos-gateway.php
@@ -5,6 +5,8 @@ Plugin Name: BML payment gateway
 Plugin URI:
 Author: Mafuth
 WC tested up to: 5.8.1
+Version: 0.0.1
+Author URI: https://mafuth.online
 Description:  This Plugin is developed by Mafuth and I am no way related to Bank of Maldives
 */
 
@@ -13,4 +15,4 @@ require_once('app/wc-bml-mpos-gateway-init.php');
 require_once('app/wc-bml-mpos-gateway-menu-column.php');
 require_once('app/functions.php');
 
- ?>
+?>
